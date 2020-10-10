76e36345647852b336c1007623cef3a4a1a78ef5
Abhilash K R <panaroma365@gmail.com>
2020-10-10 04:52:49 -0700

    added tasks and created template file

--- src/extension.ts
+++ src/extension.ts
@@ -27,23 +27,25 @@ export function activate(context: vscode.ExtensionContext) {
 			  	  enableScripts: true
 		      }
         );
-          
+        
+        const appTitle = "Messenger";
+        const appName = appTitle.toLowerCase();
         const weavy = vscode.workspace.getConfiguration("weavy");
         const weavyUrl = weavy.get<string>("url");
         const weavyUserMail = weavy.get<string>("user.mail");
         const spacesKey = weavy.get<string>("space.key");
-        const appKey = weavy.get<string>("messenger.key");
+        const appKey = weavy.get<string>(`${appName}.key`);
   
         const checkStatus = checkForMandatory(weavyUrl, weavyUserMail, spacesKey);
         
         if(!checkStatus) return;
         
         if(appKey === undefined || appKey === null || appKey === ""){
-          vscode.window.showErrorMessage(`Weavy messenger Key Cannot be empty/null. Please update a correct value in settings`);
+          vscode.window.showErrorMessage(`Weavy ${appTitle} Key Cannot be empty/null. Please update a correct value in settings`);
           return false;
         }
         
-        const content = eval("`" + getWebviewContent("messenger", context.extensionPath) + "`");
+        const content = eval("`" + getWebviewContent(context.extensionPath) + "`");
         
         currentPanel.webview.html = content;
 
@@ -57,63 +59,152 @@ export function activate(context: vscode.ExtensionContext) {
 	}});
 
 
-	// let files = vscode.commands.registerCommand('weavy.files', () => {
-	// 	const columnToShowInF = vscode.window.activeTextEditor
-  //       ? vscode.window.activeTextEditor.viewColumn
-  //       : undefined;
+	let files = vscode.commands.registerCommand('weavy.files', () => {
+		const columnToShowInF = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
 
-  //     if (currentPanel) {
-  //       currentPanel.reveal(columnToShowInF);
-  //     } else {
-  //       currentPanel = vscode.window.createWebviewPanel(
-  //         'weavyFiles',
-  //         'Weavy Files',
-  //         columnToShowInF || vscode.ViewColumn.Two,
-  //         {
-	// 			enableScripts: true
-	// 	  }
-  //       );
-  //       currentPanel.webview.html = getWebviewContent("files", context.extensionPath);
+      if (currentPanel) {
+        currentPanel.reveal(columnToShowInF);
+      } else {
+        currentPanel = vscode.window.createWebviewPanel(
+          'weavyFiles',
+          'Weavy Files',
+          columnToShowInF || vscode.ViewColumn.Two,
+          {
+				enableScripts: true
+		  }
+        );
+        
+        const appTitle = "Files";
+        const appName = appTitle.toLowerCase();
+        const weavy = vscode.workspace.getConfiguration("weavy");
+        const weavyUrl = weavy.get<string>("url");
+        const weavyUserMail = weavy.get<string>("user.mail");
+        const spacesKey = weavy.get<string>("space.key");
+        const appKey = weavy.get<string>(`${appName}.key`);
+  
+        const checkStatus = checkForMandatory(weavyUrl, weavyUserMail, spacesKey);
+        
+        if(!checkStatus) return;
+        
+        if(appKey === undefined || appKey === null || appKey === ""){
+          vscode.window.showErrorMessage(`Weavy ${appTitle} Key Cannot be empty/null. Please update a correct value in settings`);
+          return false;
+        }
+        
+        const content = eval("`" + getWebviewContent(context.extensionPath) + "`");
+        
+        currentPanel.webview.html = content;
 
-  //       currentPanel.onDidDispose(
-  //         () => {
-  //           currentPanel = undefined;
-  //         },
-  //         null,
-  //         context.subscriptions
-  //       );
-	// }});
+        currentPanel.onDidDispose(
+          () => {
+            currentPanel = undefined;
+          },
+          null,
+          context.subscriptions
+        );
+	}});
 
-	// let posts = vscode.commands.registerCommand('weavy.posts', () => {
-	// 	const columnToShowInP = vscode.window.activeTextEditor
-  //       ? vscode.window.activeTextEditor.viewColumn
-  //       : undefined;
+	let posts = vscode.commands.registerCommand('weavy.posts', () => {
+		const columnToShowInP = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
 
-  //     if (currentPanel) {
-  //       currentPanel.reveal(columnToShowInP);
-  //     } else {
-  //       currentPanel = vscode.window.createWebviewPanel(
-  //         'weavyPosts',
-  //         'Weavy Posts',
-  //         columnToShowInP || vscode.ViewColumn.Three,
-  //         {
-	// 			enableScripts: true
-	// 	  }
-  //       );
-  //       currentPanel.webview.html = getWebviewContent("posts", context.extensionPath);
+      if (currentPanel) {
+        currentPanel.reveal(columnToShowInP);
+      } else {
+        currentPanel = vscode.window.createWebviewPanel(
+          'weavyPosts',
+          'Weavy Posts',
+          columnToShowInP || vscode.ViewColumn.Three,
+          {
+				enableScripts: true
+		  }
+        );
+        
+        const appTitle = "Posts";
+        const appName = appTitle.toLowerCase();
+        const weavy = vscode.workspace.getConfiguration("weavy");
+        const weavyUrl = weavy.get<string>("url");
+        const weavyUserMail = weavy.get<string>("user.mail");
+        const spacesKey = weavy.get<string>("space.key");
+        const appKey = weavy.get<string>(`${appName}.key`);
+  
+        const checkStatus = checkForMandatory(weavyUrl, weavyUserMail, spacesKey);
+        
+        if(!checkStatus) return;
+        
+        if(appKey === undefined || appKey === null || appKey === ""){
+          vscode.window.showErrorMessage(`Weavy ${appTitle} Key Cannot be empty/null. Please update a correct value in settings`);
+          return false;
+        }
+        
+        const content = eval("`" + getWebviewContent(context.extensionPath) + "`");
+        
+        currentPanel.webview.html = content;
 
-  //       currentPanel.onDidDispose(
-  //         () => {
-  //           currentPanel = undefined;
-  //         },
-  //         null,
-  //         context.subscriptions
-  //       );
-	// }});
+        currentPanel.onDidDispose(
+          () => {
+            currentPanel = undefined;
+          },
+          null,
+          context.subscriptions
+        );
+  }});
+  
+  let tasks = vscode.commands.registerCommand('weavy.tasks', () => {
+		const columnToShowInP = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
+
+      if (currentPanel) {
+        currentPanel.reveal(columnToShowInP);
+      } else {
+        currentPanel = vscode.window.createWebviewPanel(
+          'weavyTasks',
+          'Weavy Tasks',
+          columnToShowInP || vscode.ViewColumn.Three,
+          {
+				enableScripts: true
+		  }
+        );
+        
+        const appTitle = "Tasks";
+        const appName = appTitle.toLowerCase();
+        const weavy = vscode.workspace.getConfiguration("weavy");
+        const weavyUrl = weavy.get<string>("url");
+        const weavyUserMail = weavy.get<string>("user.mail");
+        const spacesKey = weavy.get<string>("space.key");
+        const appKey = weavy.get<string>(`${appName}.key`);
+  
+        const checkStatus = checkForMandatory(weavyUrl, weavyUserMail, spacesKey);
+        
+        if(!checkStatus) return;
+        
+        if(appKey === undefined || appKey === null || appKey === ""){
+          vscode.window.showErrorMessage(`Weavy ${appTitle} Key Cannot be empty/null. Please update a correct value in settings`);
+          return false;
+        }
+        
+        const content = eval("`" + getWebviewContent(context.extensionPath) + "`");
+        
+        currentPanel.webview.html = content;
+
+        currentPanel.onDidDispose(
+          () => {
+            currentPanel = undefined;
+          },
+          null,
+          context.subscriptions
+        );
+	}});
 
   context.subscriptions.push(messenger);
-  // context.subscriptions.push(files);
-  // context.subscriptions.push(posts);
+  context.subscriptions.push(files);
+  context.subscriptions.push(posts);
+  context.subscriptions.push(tasks);
+  
 }
 
 export function deactivate() {}
--- src/utils.ts
+++ src/utils.ts
@@ -2,25 +2,12 @@ import {Uri, window, workspace} from "vscode";
 import {readFileSync} from "fs";
 import {join} from "path";
 
-export function getWebviewContent(appName: string, contextPath: string){
+export function getWebviewContent(contextPath: string){
 	let htmlPath;
-	switch(appName){
-		case "messenger":
-			htmlPath = getHtmlPathUri("messenger", contextPath);
-			break;
-		case "files":
-			htmlPath = getHtmlPathUri("files", contextPath);
-			break;
-		case "posts":
-			htmlPath = getHtmlPathUri("posts", contextPath);
-			break;
-		default:
-			htmlPath = getHtmlPathUri("a", contextPath);
-			
-	}
-	if(htmlPath === undefined)
+	htmlPath = getHtmlPathUri("template", contextPath);
+	if(htmlPath === undefined || htmlPath === null)
 		htmlPath = getHtmlPathUri("error", contextPath);
-	
+
 	console.log(htmlPath.fsPath);
 	return readFileSync(htmlPath.fsPath, "utf8");
 }
